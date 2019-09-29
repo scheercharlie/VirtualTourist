ff565776fcdd399b849f37a8d0224eea6cebf5ee
Charlie Scheer <scheer.charlie@gmail.com>
2019-09-28 21:12:58 -0600

    initial commit

---
+++ VirtualTourist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VirtualTourist
+//
+//  Created by Charlie Scheer on 9/28/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
