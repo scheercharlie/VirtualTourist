56029d3fd5a3675716a5ec4f680d7669b8e282c4
Charlie <scheersound@gmail.com>
2019-09-28 22:01:48 -0600

    Initial View
    Created the initial viewcontroller for the app

---
+++ VirtualTourist/View/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Charlie Scheer on 9/28/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+}
