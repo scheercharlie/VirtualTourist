f5dd91ba0af2b6a2d0ff157b38b0ee81a48ea5ae
Charlie <scheersound@gmail.com>
2019-09-30 13:16:30 -0600

    Get location title
    Added reverse geolocator to get the name of a location given coordinates

--- VirtualTourist/View/TravelLocationsViewController.swift
+++ VirtualTourist/View/TravelLocationsViewController.swift
@@ -60,22 +60,52 @@ class TravelLocationsViewController: UIViewController, UIGestureRecognizerDelega
         
         let point = MKPointAnnotation()
         point.coordinate = coordinate
-        point.title = "title"
+        
+        //Try to get a name for the a place given a location
+        //If successful set the point's title to the location name
+        let clLocation = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        getLocationtitle(from: clLocation) { (name, error) in
+            if let locationName = name {
+                point.title = locationName
+            } else {
+                print(error?.localizedDescription ?? "Generic Error")
+            }
+        }
         
         mapView.addAnnotation(point)
         
-        addPin(point: point)
+        addPin(point: point, name: point.title ?? "New Pin")
+    }
+    
+    //Get the name of a location from a given CLLocation
+    func getLocationtitle(from location: CLLocation, completion: @escaping (String?, Error?) -> Void) {
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            guard
+                let placemarks = placemarks, let location = placemarks.first?.name else {
+                    DispatchQueue.main.async {
+                        completion(nil, error)
+                    }
+                    return
+            }
+            DispatchQueue.main.async {
+                completion(location, nil)
+            }
+        }
     }
     
     //MARK: Active Functions
-    func addPin(point: MKPointAnnotation) {
+    func addPin(point: MKPointAnnotation, name: String) {
         let pin = Pin(context: dataController.viewContext)
         pin.latitude = point.coordinate.latitude
         pin.longitude = point.coordinate.longitude
+        pin.name = name
+        
         
         do {
             try dataController.viewContext.save()
-            print("Save Success")
+            print("Save Successful")
         } catch {
             print("could not save pin")
             presentNoActionAlert(title:"Save Failed", message:"Could not save the pin location, try again")
